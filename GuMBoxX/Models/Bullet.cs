7ef4608e403794e44d9fed4480429705fcb9a253
Aysel Abillova <ayselaa@code.edu.az>
2022-04-07 17:43:09 +0400

    initial commit

---
+++ GuMBoxX/Models/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuMBoxX.Models
+{
+    public class Bullet<T> : Base
+    {
+        public T BulletType {get; set;)
+
+         Bullet<double> bullets = new Bullet<double>();
+
+        public T[] bullet = new T[4];
+        Bullet.BulletType bulletType;   
+
+
+        
+        
+        
+
+
+       
+
+       
+      
+
+
+
+
+
+
+
+
+    }
+}
